1456204ee01b007ea692b55c751099ffe4315ac2
eric <dorseyjr000@gmail.com>
2025-01-18 20:12:17 -0600

    .\index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+let code1;
+code1 = "7+3";
+const code2 = "100/2-10";
+const code3 = "39%4*13";
+const message = "The vault has been secured. The combination is"
+console.log (message)
+const  codeA = "code1 - code2 - code3";
+console.log(codeA)
+const codeB = 'code1 - code2 - code3';
+console.log(codeB)
